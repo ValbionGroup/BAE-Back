8cb7559a614b46c711a12a8f542d9cbdcd413715
Lucas E. <espiet.l@valbion.com>
2026-03-20 15:13:36 +0100

    feat: add fast_pass model

--- app/models/event.ts
+++ app/models/event.ts
@@ -1,32 +1,32 @@
 import { EventSchema } from '#database/schema'
-import {hasMany, manyToMany} from "@adonisjs/lucid/orm";
-import Product from "#models/product";
-import type {HasMany, ManyToMany} from "@adonisjs/lucid/types/relations";
-import Member from "#models/member";
-import Job from "#models/job";
-import MemberEventAssignedJob from "#models/member_event_assigned_job";
-import PreOrder from "#models/pre_order";
-import Order from "#models/order";
+import { hasMany, manyToMany } from '@adonisjs/lucid/orm'
+import Product from '#models/product'
+import type { HasMany, ManyToMany } from '@adonisjs/lucid/types/relations'
+import Member from '#models/member'
+import Job from '#models/job'
+import MemberEventAssignedJob from '#models/member_event_assigned_job'
+import PreOrder from '#models/pre_order'
+import Order from '#models/order'
 
 export default class Event extends EventSchema {
   @manyToMany(() => Product, {
     pivotTable: 'event_products',
     pivotTimestamps: true,
-    pivotColumns: ['quantity', 'price']
+    pivotColumns: ['quantity', 'price'],
   })
   declare products: ManyToMany<typeof Product>
 
   @manyToMany(() => Member, {
     pivotTable: 'member_responses',
     pivotTimestamps: true,
-    pivotColumns: ['is_available']
+    pivotColumns: ['is_available'],
   })
   declare members: ManyToMany<typeof Member>
 
   @manyToMany(() => Job, {
     pivotTable: 'event_jobs',
     pivotTimestamps: true,
-    pivotColumns: ['count']
+    pivotColumns: ['count'],
   })
   declare jobs: ManyToMany<typeof Job>
 
--- app/models/good.ts
+++ app/models/good.ts
@@ -1,23 +1,23 @@
 import { GoodSchema } from '#database/schema'
-import {belongsTo, manyToMany} from "@adonisjs/lucid/orm";
-import Category from "#models/category";
-import type {BelongsTo, ManyToMany} from "@adonisjs/lucid/types/relations";
-import Supplier from "#models/supplier";
-import Product from "#models/product";
+import { belongsTo, manyToMany } from '@adonisjs/lucid/orm'
+import Category from '#models/category'
+import type { BelongsTo, ManyToMany } from '@adonisjs/lucid/types/relations'
+import Supplier from '#models/supplier'
+import Product from '#models/product'
 
 export default class Good extends GoodSchema {
   @belongsTo(() => Category)
-  declare category: BelongsTo<typeof Category>;
+  declare category: BelongsTo<typeof Category>
 
   @manyToMany(() => Supplier, {
     pivotTimestamps: true,
     pivotColumns: ['price'],
-    pivotTable: 'good_suppliers'
+    pivotTable: 'good_suppliers',
   })
-  declare suppliers: ManyToMany<typeof Supplier>;
+  declare suppliers: ManyToMany<typeof Supplier>
 
   @manyToMany(() => Product, {
-    pivotTable: 'product_goods'
+    pivotTable: 'product_goods',
   })
-  declare products: ManyToMany<typeof Product>;
+  declare products: ManyToMany<typeof Product>
 }
