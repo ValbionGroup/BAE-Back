c8bde72f3ad61a6a09a1c545e3ba17b4e42bb1e4
Lucas E. <espiet.l@valbion.com>
2026-03-20 15:30:37 +0100

    feat: add order model

--- app/models/order.ts
+++ app/models/order.ts
@@ -1,3 +1,20 @@
 import { OrderSchema } from '#database/schema'
+import {belongsTo, manyToMany} from "@adonisjs/lucid/orm";
+import Member from "#models/member";
+import type {BelongsTo, ManyToMany} from "@adonisjs/lucid/types/relations";
+import Event from "#models/event";
+import Product from "#models/product";
 
-export default class Order extends OrderSchema {}
+export default class Order extends OrderSchema {
+  @belongsTo(() => Member)
+  declare takenBy: BelongsTo<typeof Member>
+
+  @belongsTo(() => Event)
+  declare event: BelongsTo<typeof Event>
+
+  @manyToMany(() => Product, {
+    pivotTable: 'order_products',
+    pivotColumns: ['quantity'],
+  })
+  declare products: ManyToMany<typeof Product>
+}
