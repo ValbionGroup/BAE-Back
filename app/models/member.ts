1e58b26aadd9d0f06564d6688472da70251c3f6c
Lucas E. <espiet.l@valbion.com>
2026-03-20 15:26:24 +0100

    feat: add member model

--- app/models/member.ts
+++ app/models/member.ts
@@ -1,3 +1,37 @@
 import { MemberSchema } from '#database/schema'
+import {belongsTo, hasMany, manyToMany} from "@adonisjs/lucid/orm";
+import User from "#models/user";
+import type {BelongsTo, HasMany, ManyToMany} from "@adonisjs/lucid/types/relations";
+import Order from "#models/order";
+import Event from "#models/event";
+import Job from "#models/job";
+import MemberEventAssignedJob from "#models/member_event_assigned_job";
+import Restock from "#models/restock";
 
-export default class Member extends MemberSchema {}
+export default class Member extends MemberSchema {
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @hasMany(() => Order)
+  declare takenOrders: HasMany<typeof Order>
+
+  @manyToMany(() => Event, {
+    pivotTable: 'member_responses',
+    pivotTimestamps: true,
+    pivotColumns: ['is_available'],
+  })
+  declare responses: ManyToMany<typeof Event>
+
+  @manyToMany(() => Job, {
+    pivotTable: 'member_job_preferences',
+    pivotTimestamps: true,
+    pivotColumns: ['rank'],
+  })
+  declare preferences: ManyToMany<typeof Job>
+
+  @hasMany(() => MemberEventAssignedJob)
+  declare assigned: HasMany<typeof MemberEventAssignedJob>
+
+  @hasMany(() => Restock)
+  declare restocks: HasMany<typeof Restock>
+}
--- app/models/member_event_assigned_job.ts
+++ app/models/member_event_assigned_job.ts
@@ -1,3 +1,17 @@
 import { MemberEventAssignedJobSchema } from '#database/schema'
+import {belongsTo} from "@adonisjs/lucid/orm";
+import Job from "#models/job";
+import type {BelongsTo} from "@adonisjs/lucid/types/relations";
+import Event from "#models/event";
+import Member from "#models/member";
 
-export default class MemberEventAssignedJob extends MemberEventAssignedJobSchema {}
+export default class MemberEventAssignedJob extends MemberEventAssignedJobSchema {
+  @belongsTo(() => Job)
+  declare job: BelongsTo<typeof Job>
+
+  @belongsTo(() => Event)
+  declare event: BelongsTo<typeof Event>
+
+  @belongsTo(() => Member)
+  declare member: BelongsTo<typeof Member>
+}
