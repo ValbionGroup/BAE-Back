d82a02d58317c54aa834b4850e0dcaa48d2802c3
Lucas E. <espiet.l@valbion.com>
2026-03-20 16:06:13 +0100

    feat: add supplier model

--- app/models/supplier.ts
+++ app/models/supplier.ts
@@ -1,3 +1,17 @@
 import { SupplierSchema } from '#database/schema'
+import {hasMany, manyToMany} from "@adonisjs/lucid/orm";
+import type {HasMany, ManyToMany} from "@adonisjs/lucid/types/relations";
+import Good from "#models/good";
+import Restock from "#models/restock";
 
-export default class Supplier extends SupplierSchema {}
+export default class Supplier extends SupplierSchema {
+  @manyToMany(() => Good, {
+    pivotTimestamps: true,
+    pivotColumns: ['price'],
+    pivotTable: 'good_suppliers',
+  })
+  declare goods: ManyToMany<typeof Good>
+
+  @hasMany(() => Restock)
+  declare restocks: HasMany<typeof Restock>
+}
