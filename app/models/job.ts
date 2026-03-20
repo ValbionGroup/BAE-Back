74b569ba5854b59ba6c3af07d68acfc52f8daa63
Lucas E. <espiet.l@valbion.com>
2026-03-20 15:22:06 +0100

    feat: add job model

--- app/models/job.ts
+++ app/models/job.ts
@@ -1,3 +1,25 @@
 import { JobSchema } from '#database/schema'
+import {hasMany, manyToMany} from "@adonisjs/lucid/orm";
+import type {HasMany, ManyToMany} from "@adonisjs/lucid/types/relations";
+import Event from "#models/event";
+import MemberEventAssignedJob from "#models/member_event_assigned_job";
+import Member from "#models/member";
 
-export default class Job extends JobSchema {}
+export default class Job extends JobSchema {
+  @manyToMany(() => Event, {
+    pivotTable: 'event_jobs',
+    pivotTimestamps: true,
+    pivotColumns: ['count'],
+  })
+  declare events: ManyToMany<typeof Event>
+
+  @hasMany(() => MemberEventAssignedJob)
+  declare assigned: HasMany<typeof MemberEventAssignedJob>
+
+  @manyToMany(() => Member, {
+    pivotTable: 'member_job_preferences',
+    pivotTimestamps: true,
+    pivotColumns: ['rank'],
+  })
+  declare preferences: ManyToMany<typeof Member>
+}
