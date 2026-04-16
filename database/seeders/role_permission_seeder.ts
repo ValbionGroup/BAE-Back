91af8917e38e68722b535e889a3e96a7d3d6d0d8
Thibault LATXAGUE <thibault.latxague@exakis-nelite.com>
2026-04-16 13:57:36 +0200

    Fixed warnings

--- database/seeders/role_permission_seeder.ts
+++ database/seeders/role_permission_seeder.ts
@@ -1,11 +1,11 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
-import Permission from '#models/permission'
-import Role from '#models/role'
+// import Permission from '#models/permission'
+// import Role from '#models/role'
 
 export default class extends BaseSeeder {
   async run() {
     // Write your database queries inside the run method
-    const roles = await Role.all()
-    const permissions = await Permission.all()
+    // const roles = await Role.all()
+    // const permissions = await Permission.all()
   }
 }
