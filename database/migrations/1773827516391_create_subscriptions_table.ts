e3375451dfe10d23843f5cbad8b9a2b269c89048
Thibault LATXAGUE <thibault.latxague@exakis-nelite.com>
2026-04-21 10:40:10 +0200

    Runned pnpm run format for prettier

--- database/migrations/1773827516391_create_subscriptions_table.ts
+++ database/migrations/1773827516391_create_subscriptions_table.ts
@@ -6,12 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.timestamp('subscribed_at').notNullable()
-      table
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table
         .integer('fast_pass_id')
         .unsigned()
